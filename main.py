88e66c23d5448f63dea471d193d98fdfef1c12b1
GUI-GitHub <1736319094@qq.com>
2019-06-28 08:47:19 +0800

    部分

--- main.py
+++ main.py
@@ -9,24 +9,42 @@ screen.fill((255,255,255))
 ball=Ball()
 
 
+bu=Bullent()
+
 
 if __name__=="__main__":
     ball.rect.move_ip(240,240)
+    bu.rect.move_ip(240,240)
     speed=[0,0]
+    spb=[0,0]
+    bs=[]
+
+    g=sprite.Group()
+
     while 1:
         for i in event.get():
             if i.type==QUIT:
                 exit()
-            
             if i.type==MOUSEMOTION:
-                print(ball.rect)
+                #print(ball.rect)
                 speed=[(i.pos[0]-ball.rect.x)/10,(i.pos[1]-ball.rect.y)/10]
             if i.type==MOUSEBUTTONDOWN:
-                pass
+                spb=[(i.pos[0]-ball.rect.x)/10,(i.pos[1]-ball.rect.y)/10]
+                bu.speed=spb
             ball.move(speed)
+            
+            bu.go()
+            
 
+        g.add(ball)
+        g.add(bu)
+
+        if not((0<bu.rect[0])and(bu.rect[0]<480)and(0<bu.rect[1])and(bu.rect[1]<480)):
+            g.remove(bu)
+            print("bu kill")
         screen.fill((255,255,255))
-        sprite.Group(ball).draw(screen)
+        
+        g.draw(screen)
 
         display.update()
 
--- util.py
+++ util.py
@@ -13,12 +13,14 @@ class Ball(sprite.Sprite):
 
 
 class Bullent(sprite.Sprite):
-    def __init__(self,img="b.png",speed=0):
+    def __init__(self,img="b.png",speed=(0,0)):
         super().__init__()
         self.image=image.load(img)
         self.rect=self.image.get_rect()
         self.speed=speed
     def move(self,speed):
         self.speed=speed
+        self.rect=self.rect.move(self.speed)
+    def go(self):
         self.rect=self.rect.move(self.speed)
 
